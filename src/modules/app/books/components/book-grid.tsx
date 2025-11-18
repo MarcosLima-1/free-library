23cca367a4da5ffd52d97649e463bc8443d5cb5b
Markin <Markin-work@protonmail.com>
2025-11-17 21:20:42 -0300

    fix: book grid on mobile

--- src/modules/app/books/components/book-grid.tsx
+++ src/modules/app/books/components/book-grid.tsx
@@ -7,7 +7,7 @@ interface BookGridProps {
 
 export function BookGrid({ books }: BookGridProps) {
 	return (
-		<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl2:grid-cols-6 xl:grid-cols-5">
 			{books.map((book) => (
 				<BookCard key={book.id} bookVolume={book} />
 			))}
